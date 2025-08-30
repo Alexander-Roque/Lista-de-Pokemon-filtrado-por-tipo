52fe94310158d4512c4b182ced535fad5640a4c5
lex <josalexander25@gmail.com>
2025-08-29 20:48:17 -0500

    version 1.1

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const form = document.forms.list
 
-async function getPokemon() {
+form.addEventListener("submit", async function getPokemon(event) {
+    event.preventDefault()
     try{
         const formData = new FormData(form)
         const pokemon = formData.get("name")
-
         const URL = "https://pokeapi.co/api/v2/"
         const response = await fetch(`${URL}/pokemon/${pokemon}`)
+         if (!response.ok) throw new Error("Pokémon no encontrado")
         const pokemonData = await response.json()
-        console.log(pokemonData)
+        for (const namePokemon of pokemonData.results) {
+            console.log(namePokemon)
+        }
+        
 
     } catch (error){
         console.error(error)
     }
-    
-}
+} )
 
-getPokemon()
+function createCard (pokemon){
+    const card = document.createElement ("div")
+    
+
+    const title = document.createElement ("p")
+    title.textContent = pokemon.name
+    
+
+    const type = document.createElement ("p")
+    type.textContent = pokemon.types[0].type.name
+
+    const imagen = document.createElement ("img")
+    imagen.src = pokemon.sprites.front_default
+    
+
+    card.appendChild (title);
+    card.appendChild (type)
+    card.appendChild (imagen)    
+    document.body.appendChild (card)
+}
