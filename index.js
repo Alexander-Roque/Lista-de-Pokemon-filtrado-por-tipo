01621a3ece915664f79448b9f3439cc48c0d5f60
lex <josalexander25@gmail.com>
2025-08-29 23:12:18 -0500

    version 1.3

--- index.js
+++ index.js
@@ -7,50 +7,56 @@ form.addEventListener("submit", async function getPokemon(event) {
         const pokemon = formData.get("name")
         const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
         const response = await fetch(`${URL}/pokemon/${pokemon}`)
-         if (!response.ok) throw new Error("Pokémon no encontrado")
+        if (!response.ok) throw new Error("Pokémon no encontrado")
         const pokemonData = await response.json()
+
         for (const namePokemon of pokemonData.results) {
             const URL = namePokemon.url
             const response = await fetch(URL)
             const pokeDataIntert = await response.json()
-            createCard (pokeDataIntert)
+            const typesData = []
+
             for (const types of pokeDataIntert.types) {
                 const nameType = types.type.name
                 const URL = types.type.url
                 const response = await fetch (URL)
                 const pokeTypeData = await response.json()
-                // console.log(pokeTypeData.sprites["generation-viii"]["sword-shield"].name_icon)
-                typeImage(types)
+                const imgUrl = pokeTypeData.sprites["generation-viii"]["sword-shield"].name_icon
+                // console.log(types)
+                // if (types.slot ==! 1){
+                //     createCard (pokeDataIntert, pokeTypeData)
+                // }
+                typesData[types.slot - 1] = {name: nameType, img: imgUrl}
+                createCard(pokeDataIntert, typesData)
             }
         }
-        
     } catch (error){
         console.error(error)
     }
 } )
 
-function createCard (pokemon){
+function createCard (pokemon, typesData){
     const card = document.createElement ("div")
     
 
     const title = document.createElement ("p")
     title.textContent = pokemon.name
-    
-    function typeImage (pokeType){
-        const type = document.createElement ("p")
-        title.textContent = pokeType.name
-        const typeImg = document.createElement ("img")
-        typeImg.src = pokeType.sprites["generation-viii"]["sword-shield"].name_icon
-
-        card.appendChild (type)
-        card.appendChild (typeImg)
-    }
 
     const imagen = document.createElement ("img")
     imagen.src = pokemon.sprites.front_default
     
-
+    typesData.forEach(typeObj => {
+        if (typeObj) {
+            const type = document.createElement("p")
+            type.textContent = typeObj.name
+            const typeImg = document.createElement("img")
+            typeImg.src = typeObj.img
+            card.appendChild(type)
+            card.appendChild(typeImg)
+        }
+    })
     card.appendChild (title);
     card.appendChild (imagen);
+
     document.body.appendChild (card)
 }
