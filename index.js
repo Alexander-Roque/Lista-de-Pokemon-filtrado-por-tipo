927e97732c5d454b73314bda4420dfa284db5dee
lex <josalexander25@gmail.com>
2025-08-29 19:56:33 -0500

    version 1.0

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-async function pokemons() {
-    try {
-        const url = "https://pokeapi.co/api/v2/"
-        const response = await fetch(`${url}/pokemon`)
-        if (!response.ok) throw new Error(response.statusText)
-        const pokemonData = await response.json();
+const form = document.forms.list
+
+async function getPokemon() {
+    try{
+        const formData = new FormData(form)
+        const pokemon = formData.get("name")
+
+        const URL = "https://pokeapi.co/api/v2/"
+        const response = await fetch(`${URL}/pokemon/${pokemon}`)
+        const pokemonData = await response.json()
         console.log(pokemonData)
-} catch (error) {
-    console.error(error)
+
+    } catch (error){
+        console.error(error)
+    }
+    
 }
 
-
-}
-
-pokemons()
+getPokemon()
